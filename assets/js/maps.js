742ec6a1acc6af61751ec5ff6402a5711a5e297a
Alicepinch <alicepinch@hotmail.co.uk>
2021-01-15 19:33:08 +0000

    Google api linked, map added to page with St Tropez location set

---
+++ assets/js/maps.js
@@ -0,0 +1,13 @@
+//Google map API
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: {
+            lat: 43.26781,
+            lng: 6.640711
+        }
+    });
+}
+
+// Setting locations for different options
