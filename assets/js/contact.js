26d7973091aba0e9d6b6b8d6483dde25bbd946f9
Alicepinch <alicepinch@hotmail.co.uk>
2021-01-25 20:19:18 +0000

    Function added for succesful form completion, clear the form fields when the form is complete

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,11 +1,22 @@
 function sendMail(contactForm) {
-    emailjs.send("service_6asdbl8","stay-in-st-tropez", {
+    emailjs.send("service_6asdbl8", "stay-in-st-tropez", {
         "from_name": contactForm.fname.value,
         "from_email": contactForm.email.value,
         "contact_inquiry": contactForm.message.value,
         "subject": contactForm.subject.value,
     })
-    .then
-    alert("Thank you for getting in touch, We will get back to you soon.");
+        .then(
+            function () {
+                document.getElementById("submit-button").innerHTML = "Message Sent!";
+                alert("Thank you for getting in touch, We will get back to you soon.");
+            });
+
+    document.getElementById("subject").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("fname").value = "";
+    document.getElementById("lname").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
+
     return false;
 }
