f78f222fe12c58f9a95726c05ed47c8e747d0caa
Alicepinch <alicepinch@hotmail.co.uk>
2021-01-25 21:20:07 +0000

    Assigned variable for Submit button and updates function with variable

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,5 @@
+let submitButton = document.getElementById("submit-button");
+
 function sendMail(contactForm) {
     emailjs.send("service_6asdbl8", "stay-in-st-tropez", {
         "from_name": contactForm.fname.value,
@@ -6,17 +8,26 @@ function sendMail(contactForm) {
         "subject": contactForm.subject.value,
     })
         .then(
+
+            //Function changes the submit button when form submitted alerts user
             function () {
-                document.getElementById("submit-button").innerHTML = "Message Sent!";
+                submitButton.innerHTML = "Message Sent!";
+                submitButton.classList.add("active");
                 alert("Thank you for getting in touch, We will get back to you soon.");
+            },
+            //Function alerts user with error if the form was unsuccesful
+            function (error) {
+                alert("Sorry something, please try again", error);
             });
 
+    // Clears the form fields after form is submitted
     document.getElementById("subject").value = "";
     document.getElementById("email").value = "";
     document.getElementById("fname").value = "";
     document.getElementById("lname").value = "";
     document.getElementById("subject").value = "";
     document.getElementById("message").value = "";
+    submitButton.classList.remove("active");
 
     return false;
 }
