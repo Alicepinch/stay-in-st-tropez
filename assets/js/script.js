9b26b7e4467a6aabbe2c69b299d779cef5d029f6
Alicepinch <alicepinch@hotmail.co.uk>
2021-02-01 10:58:54 +0000

    Updated functions to be jquery to be more consitent and to shorten file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
- //Setting constant variables for buttons 
+//Setting constant variables for buttons 
 const beachReco = document.getElementById("reco-button-1");
 const restaurantReco = document.getElementById("reco-button-2");
 const vineyardReco = document.getElementById("reco-button-3");
@@ -7,10 +7,10 @@ const videoContainer = document.getElementsByClassName("video-container");
 const beachRecoPara = document.getElementById("reco-para-1");
 const restaurantRecoPara = document.getElementById("reco-para-2");
 const vineyardRecoPara = document.getElementById("reco-para-3");
-let pageHeight = 40;
+const scrollButton = document.getElementById("scroll-top");
+let pageHeight = 100;
 
-//Hides the following components when page is loaded
-
+//Hides the following components when document is ready
 $(function () {
     $(videoContainer).hide();
     $(beachRecoPara).hide();
@@ -18,55 +18,80 @@ $(function () {
     $(vineyardRecoPara).hide();
 });
 
-/// Slideshow created following the Snook tutorial https://snook.ca/archives/javascript/simplest-jquery-slideshow using jQuery
-/// Creats a slideshow of images for the header
+/* Slideshow created following the Snook tutorial https://snook.ca/archives/javascript/simplest-jquery-slideshow using jQuery */
+// Slide show fades from image to image 
 $(function () {
     $('.slideshow img:gt(0)').hide();
     setInterval(function () {
         $('.slideshow :first-child')
-            .fadeOut(3000)
+            .fadeOut(2000)
             .next('img')
-            .fadeIn(3000)
+            .fadeIn(2000)
             .end()
             .appendTo('.slideshow');
     },
-        7000);
+        6000);
 });
-
 //End of credit
 
-// Toggles content when button is clicked for youtube video and more details
-$(function () {
-    $(watchVideo).click(function () {
-        $(videoContainer).slideToggle();
-    });
+//Function changes text and colour of button when clicked for 'Watch Video CTA'
+function changeText() {
+    if ($(watchVideo).html() === "Watch Video") {
+        $(watchVideo).html("Hide Video") ;
+        $(watchVideo).addClass("active");
+    }
+    else {
+        $(watchVideo).html("Watch Video");
+         $(watchVideo).removeClass("active");
+    }
+};
 
-    $(beachReco).click(function () {
-        $(beachRecoPara).slideToggle();
-    });
+//Function changes text and colour of More Details Button's 
+function showDetails(recommendation) {
 
-    $(restaurantReco).click(function () {
-        $(restaurantRecoPara).slideToggle();
-    });
+    if ($(recommendation).html() === "More Details") {
+        $(recommendation).html("Hide Details") ;
+        $(recommendation).addClass("active");
+    }
+    else {
+        $(recommendation).html("More Details");
+        $(recommendation).removeClass("active");
+    }
 
-    $(vineyardReco).click(function () {
-        $(vineyardRecoPara).slideToggle();
-    });
+}
 
+/* Event listeners that toggle content when button is clicked for youtube video and more details and changes status of buttons */
+$(watchVideo).click(function () {
+    $(videoContainer).slideToggle();
+    changeText();
+});
+
+$(beachReco).click(function () {
+    $(beachRecoPara).slideToggle();
+    showDetails(beachReco)
+});
+
+$(restaurantReco).click(function () {
+    $(restaurantRecoPara).slideToggle();
+    showDetails(restaurantReco)
+});
+
+$(vineyardReco).click(function () {
+    $(vineyardRecoPara).slideToggle();
+    showDetails(vineyardReco)
 });
 
 //End of jQuery
 
+//Javascript starts
+// On scroll button created and adjusted from following WS3schools tutorial
 //Assigns the scroll function to window scroll
 window.onscroll = function () {
     scrollToTop();
-}
+};
 
 //Function displays the button if the window is less than 40px 
 function scrollToTop() {
-
-    //Assigns scroll button to variable
-    var scrollButton = document.getElementById("scroll-top");
 
     if (document.body.scrollTop > pageHeight || document.documentElement.scrollTop > pageHeight) {
         scrollButton.style.display = "block";
@@ -74,56 +99,10 @@ function scrollToTop() {
         scrollButton.style.display = "none";
     }
 
-};
+}
 
 //Function sets the location as 0px from top of the page to be called by button
 function topOfPage() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-};
-
-
-//Function changes text and colour of button when clicked for 'Watch Video CTA'
-function changeText() {
-    if (watchVideo.innerHTML === "Watch Video") {
-        watchVideo.innerHTML = "Hide Video";
-        watchVideo.classList.add("active");
-    }
-    else {
-        watchVideo.innerHTML = "Watch Video";
-        watchVideo.classList.remove("active");
-    }
-};
-
-//Event listener for when customer clicks on the 'watch video' cta
-watchVideo.addEventListener("click", function () {
-    changeText()
-});
-
-//Function that changes text and colour of button 
-function showDetails(recommendation) {
-
-    if (recommendation.innerHTML === "More Details") {
-        recommendation.innerHTML = "Hide Details";
-        recommendation.classList.add("active");
-    }
-    else {
-        recommendation.innerHTML = "More Details";
-        recommendation.classList.remove("active");
-    }
-
-};
-
-
-//Event listeners for the 'More Details' Buttons
-beachReco.addEventListener("click", function () {
-    showDetails(beachReco);
-});
-
-restaurantReco.addEventListener("click", function () {
-    showDetails(restaurantReco);
-});
-
-vineyardReco.addEventListener("click", function () {
-    showDetails(vineyardReco);
-});
+}
