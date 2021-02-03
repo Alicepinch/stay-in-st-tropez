427743ad8303febdebfe0386665465c748092af6
Alicepinch <alicepinch@hotmail.co.uk>
2021-02-03 16:42:38 +0000

    Simplified Javascript for displaying details using jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,17 @@
-//Setting constant variables for buttons 
-const beachReco = document.getElementById("beach-reco-btn");
-const restaurantReco = document.getElementById("restaurant-reco-btn");
-const vineyardReco = document.getElementById("vineyard-reco-btn");
-const watchVideo = document.getElementById("youtube-video");
+//Setting constant variables for buttons and containers
+const detailsButton = document.getElementsByClassName("reco-button")
+const watchVideo = document.getElementById("video-btn");
 const videoContainer = document.getElementsByClassName("video-container");
-const beachRecoPara = document.getElementById("beach-para");
-const restaurantRecoPara = document.getElementById("restaurant-para");
-const vineyardRecoPara = document.getElementById("vineyard-para");
-const scrollButton = document.getElementById("scroll-top");
-const scrollHeight = 200;
+const moreDetails = document.getElementsByClassName("more-details")
 
 //Hides the following components when document is ready
 $(function () {
     $(videoContainer).hide();
-    $(beachRecoPara).hide();
-    $(restaurantRecoPara).hide();
-    $(vineyardRecoPara).hide();
+    $(moreDetails).hide();
 });
 
 /* Slideshow created following the Snook tutorial https://snook.ca/archives/javascript/simplest-jquery-slideshow using jQuery */
-// Slide show fades from image to image 
+// Function fades in and out from different images producing a slideshow effect
 $(function () {
     $('.slideshow img:gt(0)').hide();
     setInterval(function () {
@@ -34,61 +26,43 @@ $(function () {
 });
 //End of credit
 
-//Function changes text and colour of button when clicked for 'Watch Video CTA'
-function changeText() {
-    if ($(watchVideo).html() === "Watch Video") {
-        $(watchVideo).html("Hide Video");
-        $(watchVideo).addClass("active");
-    }
-    else {
-        $(watchVideo).html("Watch Video");
-        $(watchVideo).removeClass("active");
-    }
-}
-
-//Function changes text and colour of More Details Button's 
-function showDetails(recommendation) {
-
-    if ($(recommendation).html() === "More Details") {
-        $(recommendation).html("Hide Details");
-        $(recommendation).addClass("active");
-    }
-    else {
-        $(recommendation).html("More Details");
-        $(recommendation).removeClass("active");
-    }
-
-}
-
-/* Event listeners that toggle content when button is clicked for youtube video and more details and changes status of buttons */
+//Click events for the "Watch Video" CTA
 $(watchVideo).click(function () {
-    $(videoContainer).slideToggle();
-    changeText();
+    $(this).siblings(".video-container").slideToggle()
+
+    if ($(this).html() === "Watch Video") {
+        $(this).html("Hide Video");
+        $(this).addClass("active");
+    } else {
+        $(this).html("Watch Video");
+        $(this).removeClass("active");
+    }
 });
 
-$(beachReco).click(function () {
-    $(beachRecoPara).slideToggle();
-    showDetails(beachReco);
-});
+//On click events for the "More Details CTA's"
+$(detailsButton).click(function () {
+    $(this).siblings('.more-details').slideToggle();
 
-$(restaurantReco).click(function () {
-    $(restaurantRecoPara).slideToggle();
-    showDetails(restaurantReco);
-});
-
-$(vineyardReco).click(function () {
-    $(vineyardRecoPara).slideToggle();
-    showDetails(vineyardReco);
+    if ($(this).html() === "More Details") {
+        $(this).html("Hide Details");
+        $(this).addClass("active");
+    } else {
+        $(this).html("More Details");
+        $(this).removeClass("active");
+    }
 });
 
 //End of jQuery
 
 // On scroll button created and adjusted from following WS3schools tutorial
-//Assigns the scroll function to window scroll
+//Assigning the scroll function to window on scroll
 window.onscroll = scrollToTop;
 
-//Function displays the button to user when they have scolled 200px from top of page
+//Function displays the button to user when they have scolled 200px from top
 function scrollToTop() {
+
+    let scrollHeight = 200;
+    let scrollButton = document.getElementById("scroll-top");
 
     if (document.body.scrollTop > scrollHeight || document.documentElement.scrollTop > scrollHeight) {
         scrollButton.style.display = "block";
@@ -98,7 +72,7 @@ function scrollToTop() {
 
 }
 
-//Function sets the location as 0px from top of the page to be called by button
+/* Function sets the location as 0px from top of the page. This function is called on the "Scroll-top" button in the index.html file */
 function topOfPage() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
