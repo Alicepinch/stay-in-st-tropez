223163021e174d599f1886a03cfb8e0de5fbdb7a
Alicepinch <alicepinch@hotmail.co.uk>
2021-02-09 16:48:49 +0000

    Form alert fixed

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,4 +1,7 @@
+
+
 function sendMail(contactForm) {
+
     emailjs.send("service_6asdbl8", "stay-in-st-tropez", {
         "from_name": contactForm.fname.value,
         "from_email": contactForm.email.value,
@@ -6,19 +9,18 @@ function sendMail(contactForm) {
         "subject": contactForm.subject.value,
     })
         .then(
-
             //Function changes the submit button when form submitted alerts user
             function () {
                 alert("Thank you for getting in touch, We will get back to you soon.");
+                document.getElementById("contact-form").reset(); 
             },
-
             //Function alerts user with error if the form was unsuccesful
             function (error) {
                 alert("Sorry something, please try again", error);
             });
 
     // Clears the form fields after form is submitted
-    document.getElementById("contact_form").reset();
+    
 
     return false;
 }
